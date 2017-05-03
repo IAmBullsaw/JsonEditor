07c02c9b989c3cf52dfa50b3a73cecef9095dc01
Oskar Jansson <oskja067@student.liu.se>
2017-05-03 10:10:04 +0200

    adds: added all cards constans

--- constants.h
+++ constants.h
@@ -2,5 +2,9 @@
 #define CONSTANTS_H
 
 static QString c_title{"_title"};
+static QString c_description{"_description"};
+static QString c_id{"_id"};
+static QString c_images{"_images"};
+static QString c_text{"_text"};
 
 #endif // CONSTANTS_H
